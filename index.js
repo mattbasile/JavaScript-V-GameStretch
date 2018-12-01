3457f324667cdea1c5ed4790340d3ef0af7bf870
Matt Basile <matthewbasile2@gmail.com>
2018-12-01 12:32:03 -0500

    Restarting w a DragonBall-Z Theme in mind. Reference new repository for updates.

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ class Characters {
         this.secretMove = attributes.secretMove;
         this.image = attributes.image;
         this.healCount = attributes.healCount;
+        this.power = attributes.power;
     }
-    attack(){
-        
-    }
-    special(){
-
-    }
+    attack(player, wpn){
+       let newhealth = player.health - this.weapon[wpn];
+       player.health = newhealth
+       if(newhealth <= 0){
+         return `${player.name} has died. Play Again?`
+       }
+        return `${this.name} attacked ${player.name} for ${this.weapon[wpn]} damage. ${player.name} health is now at ${player.health}`
+       }
+    special(player, sct){
+        console.log(this.power);
+        console.log(this.secretMove);
+        let newhealth = player.health - this.secretMove[sct];
+        player.health = newhealth
+       if(newhealth <= 0){
+         return `${player.name} has died. Play Again?`
+       }
+        return `${this.name} attacked ${player.name} for ${this.secretMove[sct]} damage. ${player.name} health is now at ${player.health}`
+       }
+    
     heal(){
-
     }
 }
 class mainChar extends Characters{
@@ -69,12 +82,11 @@ const harpy = new Characters({
     name: "Harpy",
     health: 150,
     weapon: {
-        "Silver Beak": 15,
+        "SilverBeak": 15,
     },
+    power: 5,
     secretMove: {
-        "Lust to Death": function(){
-            return Math.floor(Math.random() * Math.floor(30))
-        }
+        "Lust to Death": Math.floor(Math.random() * Math.floor(30)),
     },
     image: "img/harpy.png",
     healCount: 3,
@@ -83,12 +95,10 @@ const grunt = new Characters({
     name: "Grunt",
     health: 250,
     weapon: {
-        "Chain Saw": 10,
+        "ChainSaw": 10,
     },
     secretMove: {
-        "Timber!": function(){
-            return Math.floor(Math.random() * Math.floor(20))
-        }
+        "Timber!": Math.floor(Math.random() * Math.floor(20)),   
     },
     image: "img/grunt.jpg",
     healCount: 3,
@@ -101,11 +111,14 @@ const team = {
 }
 
 function selectCharMain(){
+   
    const key = this.innerHTML.toLowerCase();
    const char = team[key];
+   document.querySelector(".main")
    document.querySelector(".charImgOne").src=`${char.image}`;
    document.querySelector(".main-title").innerHTML=`Warrior: ${char.name}`;
    document.querySelector(".main .health").innerHTML=`Health:${char.health}`;
+
 }
 
 function selectCharSec(){
@@ -116,18 +129,7 @@ function selectCharSec(){
     document.querySelector(".secondary .health").innerHTML=`Health:${char.health}`;   
  }
 
- function attack(){
-     console.log(this.parentElement.parentElement)
-     if(this.parentElement.parentElement == document.querySelector(".main")){
-        this.call(team);
-        console.log(this)
-        let opponent = document.querySelector(".secondary . health");
-        
-        
-     } else{
-         console.log(``)
-     }
- }
-
+//  console.log(harpy.attack(grunt, "SilverBeak"));
+ console.log(harpy.special(grunt, "Lust to Death"));
  
 
